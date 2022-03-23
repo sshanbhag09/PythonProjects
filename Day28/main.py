f365de0f9d77baf17892527301d2ef5165bf7b43
sshanbhag09 <69388704+sshanbhag09@users.noreply.github.com>
2022-03-23 08:20:26 +0530

    Add files via upload

---
+++ Day28/main.py
@@ -0,0 +1,6 @@
+# height=float(input("Enter the height"))
+# weight=float(input("Enter the weight"))
+# if height>3:
+#     raise ValueError("Human Height does  not exceed 3 m")
+# bmi=weight/height**2
+# print(bmi)
